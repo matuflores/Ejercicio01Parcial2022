1ac120a745d58226a22be43b596ca415faa2fd22
matia <matia@MATIFLORES>
2022-07-11 22:09:26 -0300

    Agregar archivos de proyecto.

---
+++ FechaEJ01.Consola/Program.cs
@@ -0,0 +1,62 @@
+﻿using FechaEJ01.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechaEJ01.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, anio;
+
+            Fecha hoy = "11/02/2020";
+            Fecha sistema = new Fecha();
+
+            Console.Write("Ingrese el dia: ");
+            dia = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el mes: ");
+            mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el anio: ");
+            anio = int.Parse(Console.ReadLine());
+
+            
+            var fecha1 = new Fecha(dia, mes, anio);
+            var fecha2 = new Fecha(26, 7, 1996);
+            var fecha3 = new Fecha(21, 12, 1998);
+            var fecha4 = new Fecha(21, 12, 1998);
+
+            if (fecha1.Validar())
+            {
+                Console.WriteLine($"Fecha validada: {fecha1.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine("Fecha mal ingresada");
+            }
+
+            //Inicializar el constructor con las fecha de hoy
+            Console.WriteLine($"Fecha del sistema {sistema.ToString()}");
+
+            //Convertir de String a Fecha
+            Console.WriteLine($"La fecha ingresada en string es {hoy.ToString()}");
+
+            //Prueba del equals
+            if (fecha4 == fecha3)
+            {
+                Console.WriteLine("Ambas fechas son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Las fechas son distintas");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FechaEJ01.Entidades/Fecha.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechaEJ01.Entidades
+{
+    public class Fecha
+    {
+        private int Dia;
+        private int Mes;
+        private int Anio;
+
+        public Fecha(int dia, int mes, int anio)
+        {
+            this.Dia = dia;
+            this.Mes = mes;
+            this.Anio = anio;
+        }
+
+        public Fecha()
+        {
+            Dia = DateTime.Now.Day;
+            Mes = DateTime.Now.Month;
+            Anio = DateTime.Now.Year;
+        }
+        public bool Validar()
+        {
+            if (Dia > 0 && Mes >= 1 && Mes <= 12)
+            {
+                if (Mes == 1 || Mes == 3 || Mes == 5 || Mes == 7 || Mes == 8 || Mes == 10 || Mes == 12)
+                {
+                    return Dia <= 31;
+                }
+
+                if (Mes == 4 || Mes == 6 || Mes == 9 || Mes == 11)
+                {
+                    return Dia <= 30;
+                }
+                if (Mes == 2)
+                {
+                    return Dia <= 29;
+                }
+            }
+            return false;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Dia.ToString().PadLeft(2, '0')}/{Mes.ToString().PadLeft(2, '0')}/{Anio.ToString().PadLeft(4, '0')}";
+        }
+
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Fecha))
+            {
+                return false;
+            }
+
+            return this.Dia == ((Fecha)obj).Dia &&
+                   this.Mes == ((Fecha)obj).Mes &&
+                   this.Anio == ((Fecha)obj).Anio;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(Fecha f1, Fecha f2)
+        {
+            return f1.Equals(f2);
+        }
+
+        public static bool operator !=(Fecha f1, Fecha f2)
+        {
+            return !(f1 == f2);
+        }
+
+        public static implicit operator Fecha(string hoy)
+        {
+            int dia, mes, anio;
+            var campos = hoy.Split('/');
+            dia = int.Parse(campos[0]);
+            mes = int.Parse(campos[1]);
+            anio = int.Parse(campos[2]);
+
+            return new Fecha(dia, mes, anio);
+        }
+    }
+}
